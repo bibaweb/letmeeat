d40e95922ef13deb5aec7cb174db0aa5f7bed09e
Anastasia S <anastja.s@gmail.com>
2020-12-11 13:52:08 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  const APP_ID = '3081082d';
+  const APP_KEY = '6a9b68deb5fcaf325263757bfefa0b8f';
+  const exampleRequest = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="recipes">
+      <h1 className="recipes__title">Рецепты на сегодня</h1>
+      <form className="recipes__search-form">
+        <input className="recipes__search-bar" type="text"/>
+        <button className="recipes__search-buton" type="submit"></button>
+      </form>
     </div>
   );
 }
